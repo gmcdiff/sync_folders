4b4352af1d728c052f3c26ecf804ad92cc7c2d8a
Joevann <78760792+gmcdiff@users.noreply.github.com>
2023-12-22 12:14:01 +0000

    [Refactor] sync_folder.py

--- sync_folder.py
+++ sync_folder.py
@@ -4,10 +4,19 @@ import argparse
 import time
 from datetime import datetime
 
+def log(log_file, message):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log_entry = f"{timestamp} - {message}"
+
+    with open(log_file, "a") as lf:
+        lf.write(log_entry + "\n")
+        
 def synchronize_folders(source_folder, replica_folder, log_file):
     if not os.path.exists(replica_folder):
         os.makedirs(replica_folder)
-
+        log(log_file, f"Created replica folder: {replica_folder}")
+        print(f"Created replica folder: {replica_folder}")
+        
     for root, dirs, files in os.walk(source_folder):
         relative_path = os.path.relpath(root, source_folder)
         replica_path = os.path.join(replica_folder, relative_path)
@@ -27,21 +36,16 @@ def synchronize_folders(source_folder, replica_folder, log_file):
                     shutil.copy2(source_file_path, replica_file_path)
                     log(log_file, f"Copied: {source_file_path} to {replica_file_path}")
                     print(f"Copied: {source_file_path} to {replica_file_path}")
-
+                else:
+                    log(log_file, f"File already up to date: {replica_file_path}")
+                    print(f"File is already up to date: {replica_file_path}")
             else:
                 shutil.copy2(source_file_path, replica_file_path)
                 log(log_file, f"Copied: {source_file_path} to {replica_file_path}")
                 print(f"Copied: {source_file_path} to {replica_file_path}")
 
-def log(log_file, message):
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    log_entry = f"{timestamp} - {message}"
-
-    with open(log_file, "a") as lf:
-        lf.write(log_entry + "\n")
-
 if __name__ == "__main__":
-    print("Script is running...")
+    print("Synchronization in process...")
     parser = argparse.ArgumentParser(description="Folder Synchronization")
     parser.add_argument("source", help="Path to the source folder")
     parser.add_argument("replica", help="Path to the replica folder")
